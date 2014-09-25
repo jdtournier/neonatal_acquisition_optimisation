309c8d85afb61fa1bc57eb9357593907038d7856
J-Donald Tournier <jdtournier@gmail.com>
2014-09-25 19:32:28 +0100

    Presentation ready

--- plot_responses.m
+++ plot_responses.m
@@ -4,7 +4,12 @@ function plot_responses (param, file_prefix)
 
 scale = 2*(sum(param.weights, 2) > 0)-1;
 for n = 1:size(param.response,2)
-  plot (param.bvals_HR, scale(n)*param.response(:,n));
+  h = plot (param.bvals_HR, scale(n)*param.response(:,n));
   grid on;
+  set (gca, 'color', 'none', 'xcolor', 'white', 'ycolor', 'white');
+  set(findobj('type', 'line'), 'color', 'white');
+  set(findobj('type', 'text'), 'color', 'white');
+  set (h, 'color', [1 1 0], 'linewidth', 2);
+  set(gcf, 'invertHardcopy', 'off', 'color', 'none')
   printpdf (gcf, [ file_prefix num2str(n) ]);
 end
