fb98b21d546057759c843af94ea69f16a22d9594
J-Donald Tournier <jdtournier@gmail.com>
2017-02-02 10:44:51 +0000

    stash results using EDDY 5.0.8

--- write_responses.m
+++ write_responses.m
@@ -1,6 +1,6 @@
-function write_responses (param, filename)
+function write_responses (param, weights_filename, responses_filename)
 
-% write_responses (param, filename)
+% write_responses (param, weights_filename)
 %
 % save the response weights to a mif file
 
@@ -9,5 +9,7 @@ image = param.header;
 scale = 2*(sum(param.weights, 2) > 0)-1;
 weights = bsxfun (@times, param.weights, scale);
 image.data = unvec0 (weights, param.mask);
-write_mrtrix (image, filename);
+write_mrtrix (image, weights_filename);
+R = param.response;
+save -ascii responses.txt R;
 
